17bf543b9e6d4e80b26b7f6ed21aeedcd01a5d74
JarvisSemou <mouse.jiangwei@qq.com>
2020-05-31 18:56:13 +0800

    Site updated: 2020-05-31 18:56:12

--- js/main.js
+++ js/main.js
@@ -17,9 +17,9 @@ function pageScroll(){
 		start_hight = end_hight;
 		var $header = $('#header');
         if(distance > 0 && end_hight > 50){
-			$header.hide();
+			//$header.hide();
 		}else if(distance < 0){
-			$header.show();
+			//$header.show();
 		}else{
 			return false;
 		}
